d090e14eda6ff02ef00faa7fc3b0ab5903baf441
Annosz <tadamleanyfalu@gmail.com>
2021-01-09 19:28:40 +0100

    Try SameSite Cookie Changes
    Based on https://devblogs.microsoft.com/aspnet/upcoming-samesite-cookie-changes-in-asp-net-and-asp-net-core/

--- EU4AchievementChecklist/Startup.cs
+++ EU4AchievementChecklist/Startup.cs
@@ -4,6 +4,7 @@ using EU4AchievementChecklist.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,6 +18,17 @@ namespace EU4AchievementChecklist
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // Same site cookie policy edits needed for external OpenIdConnect authentication
+            // Based on https://devblogs.microsoft.com/aspnet/upcoming-samesite-cookie-changes-in-asp-net-and-asp-net-core/
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                options.OnAppendCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+                options.OnDeleteCookie = cookieContext =>
+                    CheckSameSite(cookieContext.Context, cookieContext.CookieOptions);
+            });
+
             services
                 .AddAuthentication(options =>
                     {
@@ -50,6 +62,8 @@ namespace EU4AchievementChecklist
                 app.UseReverseProxyHttpsEnforcer();
             }
 
+            app.UseCookiePolicy();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -62,6 +76,42 @@ namespace EU4AchievementChecklist
                 endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
+        }
+
+        private void CheckSameSite(HttpContext httpContext, CookieOptions options)
+        {
+            if (options.SameSite == SameSiteMode.None)
+            {
+                var userAgent = httpContext.Request.Headers["User-Agent"].ToString();
+                if (DisallowsSameSiteNone(userAgent))
+                {
+                    options.SameSite = SameSiteMode.Unspecified;
+                }
+            }
+        }
+
+        private static bool DisallowsSameSiteNone(string userAgent)
+        {
+            // IOS
+            if (userAgent.Contains("CPU iPhone OS 12") || userAgent.Contains("iPad; CPU OS 12"))
+            {
+                return true;
+            }
+
+            // Mac OS
+            if (userAgent.Contains("Macintosh; Intel Mac OS X 10_14") &&
+                userAgent.Contains("Version/") && userAgent.Contains("Safari"))
+            {
+                return true;
+            }
+
+            // Chrome
+            if (userAgent.Contains("Chrome"))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
